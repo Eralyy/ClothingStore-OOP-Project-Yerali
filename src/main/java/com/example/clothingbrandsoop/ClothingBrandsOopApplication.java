6cdeb103f9ad7f5c23411035ca5e9c5be297e87c
Eralyy <karkinbaev2007@gmail.com>
2025-12-16 16:08:35 +0500

    init commit

---
+++ src/main/java/com/example/clothingbrandsoop/ClothingBrandsOopApplication.java
@@ -0,0 +1,13 @@
+package com.example.clothingbrandsoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothingBrandsOopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClothingBrandsOopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/clothingbrandsoop/ClothingBrandsOopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clothingbrandsoop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothingBrandsOopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
